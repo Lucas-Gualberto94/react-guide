217a1066622e0acf087a0eccb5a20c93cec33035
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-01-29 21:17:08 +0000

    test1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React x
+          Learn React xxx
         </a>
       </header>
     </div>
